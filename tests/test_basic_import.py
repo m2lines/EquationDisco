b4e24d9930dfa395700aec882314c61c5967996d
jdenholm <j.denholm.2017@gmail.com>
2023-02-17 10:13:18 +0000

    :white_check_mark: Added basic import test

---
+++ tests/test_basic_import.py
@@ -0,0 +1,5 @@
+"""Basic test to import the package."""
+
+def test_package_import():
+    """Test the package imports."""
+    import eqn_disco #pylint: disable=unused-import,import-outside-toplevel
