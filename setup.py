73235490f14c13cc0cef1ab2231a8124ddd190ec
jdenholm <j.denholm.2017@gmail.com>
2023-02-17 10:05:07 +0000

    :wrench: Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+"""Setup for `TorchTools`."""
+from setuptools import setup
+
+setup(
+    name="eqn_disco",
+    package_dir={"eqn_disco": "eqn_disco"},
+    author="To Add.",
+    version="0.0.0",
+)
